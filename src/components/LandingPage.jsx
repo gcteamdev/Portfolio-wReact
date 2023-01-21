e2cd7d898aaaaec92d3e41b0be5bf79813c923e5
GC <contact@gcteam.dev>
2023-01-20 17:25:31 -0800

    added live deployment for landing page project

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,17 +6,17 @@ import "./LandingPage.css";
 function LandingPage() {
   return (
     <div className= "landingPage">
-        <div className="taskiry-outer d-lg-none d-md-block mobile-todo">
-        <h1 className="t">Taskiary</h1>
-        <h2 className="mobile-descriptionTodo">Project management app! </h2>
+        <div className="LandingPage-headerCtn">
+        <h1 className="LandingPage-header">Landing Page </h1>
+        <h2 className="">A Senior Living Home | Landing page project with React.js </h2>
         <br/>
-        <h5 className="mobile-todo-tech">Tech Stack: React.JS | Node.JS</h5>
+        <h5 className="">Tech Stack: React.JS | Boortrap | Material UI</h5>
         </div>
         <img className="landingPageImg d-lg-none"src={landingpage} alt="landingpage"/>
         <img className="landingPageLg d-lg-block d-none"src={landingpageLg} alt="landingpage"/>
         <div className="landingCode">
         <a className="source-code-btn btn btn-lg text-dark bg-white" target='_blank' href="https://github.com/gcteamdev/LandingPage-Senior.Home"> Source Code </a>
-        <a className="watch-demo-btn  btn btn-lg btn-dark"  href="#" > Watch Demo</a>
+        <a className="watch-demo-btn  btn btn-lg btn-dark landingPageLive"  href="https://landingpage-seniorliving.netlify.app/"  target="_blank"> Watch Live</a>
 
         </div>
     </div>
