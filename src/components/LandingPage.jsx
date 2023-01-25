b2464c31c9fcafc10511a7c00456186091b496bf
GC <contact@gcteam.dev>
2023-01-24 18:50:34 -0800

    responsiveness implemented for notebook media

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -14,9 +14,9 @@ function LandingPage() {
         </div>
         <img className="landingPageImg d-lg-none"src={landingpage} alt="landingpage"/>
         <img className="landingPageLg d-lg-block d-none"src={landingpageLg} alt="landingpage"/>
-        <div className="landingCode ">
-        <a className="source-code-btn btn btn-lg text-dark bg-white" target='_blank' href="https://github.com/gcteamdev/LandingPage-Senior.Home"> Source Code </a>
-        <a className="watch-demo-btn  btn btn-lg btn-dark landingPageLive"  href="https://landingpage-seniorliving.netlify.app/"  target="_blank"> Watch Live</a>
+        <div className="landingCode-btns ">
+        <a className="source-code-btn btn btn-lg text-dark bg-white mb-5" target='_blank' href="https://github.com/gcteamdev/LandingPage-Senior.Home"> Source Code </a>
+        <a className="watch-demo-btn  btn btn-lg btn-dark landingPageLive mb-5"  href="https://landingpage-seniorliving.netlify.app/"  target="_blank"> Watch Live</a>
 
         </div>
     </div>
